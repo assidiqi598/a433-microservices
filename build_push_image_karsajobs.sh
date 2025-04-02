5ebb17716bdb4dc5a015b9cd4f4c4112d93fae8a
vassidiqi <vassidiqi@gmail.com>
2025-04-02 00:06:54 +0000

    edit build command

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -6,7 +6,7 @@ IMAGE_NAME="karsajobs"
 IMAGE_TAG="latest"
 
 # Build a docker image based on Dockerfile in this directory for linux/amd64 architecture
-docker buildx build --platform linux/amd64 -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG .
+docker build -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG .
 
 # Authenticate with GitHub Packages
 echo $GH_TOKEN | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
